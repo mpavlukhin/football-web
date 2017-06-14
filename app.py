dcd15af1f04dbdebae83e1b2cf36ee1f0081d8a4
Glathor <glathor23@gmail.com>
2017-06-14 15:00:42 +0300

    Add spreadsheet file, that get info from google doc

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import spreadsheet
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.debug = True
