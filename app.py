47e430ffd8c999d4c05f1f8f444c1e9208ff93a0
Max Pavlukhin <pavlukhinm@gmail.com>
2017-06-15 16:11:18 +0400

    Using gspread for accessing Spreadsheets on Google Drive

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-import spreadsheet
+import spreadsheet as ss
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello World!"
+
+
+@app.route("/test")
+def spreadsheet():
+    return ss.getStatsArray()
 
 if __name__ == '__main__':
     app.debug = True
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,5 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pprint
 
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
@@ -7,5 +8,34 @@ client = gspread.authorize(creds)
 
 sheet = client.open('football').sheet1
 
-footballs = sheet.get_all_records()
-print(footballs)
+pp = pprint.PrettyPrinter()
+
+# result = sheet.col_values(6)
+# pp.pprint(result)
+
+#values_list = sheet.col_values(1)
+#pp.pprint(values_list)
+#footballs = sheet.get_all_records()
+#pp.pprint(footballs)
+
+# i = 1
+# while (sheet.cell(i, 0)):
+#     result = sheet.row_values(6)
+#     # pp.pprint(result))
+
+
+def getStatsArray():
+    i = 2
+    result = sheet.cell(i, 1).value
+    listOfNames = list()
+
+    while not ('-' in result):
+        result = sheet.cell(i, 1).value
+        # listOfNames.append(listOfNames, result)
+        print(result)
+        i += 1
+
+    return listOfNames
+
+
+getStatsArray()
