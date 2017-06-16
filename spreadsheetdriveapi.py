b0fda4d7672794211c8a3350cceda4c3c665d643
Max Pavlukhin <pavlukhinm@gmail.com>
2017-06-16 12:55:31 +0400

    Fixed some bugs

--- spreadsheetdriveapi.py
+++ spreadsheetdriveapi.py
@@ -17,25 +17,25 @@ mimetypes = {
 drive = GoogleDrive(gauth)
 # Auto-iterate through all files that matches this query
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-print('All your files')
+print('All your spreadsheet files on your Google Drive')
 
 for file1 in file_list:
     if file1['mimeType'] in mimetypes:
         print('File: %s' % (file1['title']))
 
-fileTitle = input('Input title file, which you want to download with extension: ')
+fileTitle = input('Input spreadsheet name which you want to download: ')
 donwloadornot = False
 for file1 in file_list:
     download_mimetype = None
     if file1['mimeType'] in mimetypes:
         download_mimetype = mimetypes[file1['mimeType']]
         if file1['mimeType'] in mimetypes and file1['title'] == fileTitle:
-            file1.GetContentFile(fileTitle + '.xlsx', mimetype=download_mimetype)
+            file1.GetContentFile('data/spreadsheets/' + fileTitle + '.xlsx', mimetype=download_mimetype)
             print('Successfully downloaded')
             donwloadornot = True
             break
 if donwloadornot == False:
-    print('Error, wrong extension')
+    print('Error, wrong name')
 
 
 
