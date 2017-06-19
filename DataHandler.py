9574094b862c2bd21c0de4e3ceb4af1a6189994f
Max Pavlukhin <pavlukhinm@gmail.com>
2017-06-19 16:19:27 +0400

    Add findActiveSheets function (it finds pages by year)

--- DataHandler.py
+++ DataHandler.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import openpyxl as pyxl
 
-
-
+# years_included = ('15', '14')
+years_included = ['17']
 
 def preCreateStatsSheet(sheetname, filepath):
     wb = pyxl.load_workbook(filepath)
@@ -12,12 +12,38 @@ def preCreateStatsSheet(sheetname, filepath):
     stat.append([''])
     wb.save(filename=filepath)
 
+def findActiveSheets(filepath):
+    wb = pyxl.load_workbook(filepath)
 
+    ws = wb.get_sheet_names()
+
+    activeSheets = list()
+    for sheet in ws:
+        if(isYearOK(sheet)):
+            activeSheets.append(sheet)
+    print("Total number of all sheets = " + str(activeSheets.__len__()))
+
+    print("This is pages:")
+    for sheetName in activeSheets:
+        print (sheetName)
+
+    return sheetName
+
+def isYearOK(year):
+    if (years_included == None):
+        raise 'No selected years Error'
+
+    for years in years_included:
+        if (year.rfind(years) != -1):
+            return True
+
+    return False
 
 def getdataframefromfile(fileTitle):
     filepath = 'data/spreadsheets/' + fileTitle + '.xlsx'
     data = pd.read_excel(filepath)
-    preCreateStatsSheet("NewStats", filepath)
+    # preCreateStatsSheet("NewStats", filepath)
+    findActiveSheets(filepath=filepath)
     return data
 
 getdataframefromfile('football')
