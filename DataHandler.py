452112599112b43d1cd5f2af7d2a2072bdce4106
Max Pavlukhin <pavlukhinm@gmail.com>
2017-06-21 17:27:29 +0400

    AnalyzePerson function was implemented with TestData. Waiting for real values from getColumnNamesRange function

--- DataHandler.py
+++ DataHandler.py
@@ -2,21 +2,19 @@ import pandas as pd
 import openpyxl as pyxl
 import numpy as np
 
-years_included = ['12', '13']
+years_included = ['13']
 newStats = 'NewStats'
 
 def getdataframefromfile(fileTitle):
     filepath = 'data/spreadsheets/' + fileTitle + '.xlsx'
 
-    rangevalues = np.array([]   )
+    rangevalues = np.array([])
 
     preCreateStatsSheet(newStats, filepath)
     activeSheets = findActiveSheets(filepath)
     persons = fillPersons(activeSheets, filepath, rangevalues)
 
-    for person in persons:
-        # print(person + ' is analyzing...')
-        analyzePerson(person, filepath)
+    analyzePersons(persons, filepath)
 
     data = pd.read_excel(filepath)
     return data
@@ -102,8 +100,6 @@ def getColumnNamesRange(currentSheet, filepath, rangevalues):
     titleyear = (int)(titledata[-2:])
     titlemonth = (int)(titledata.replace(' ', '')[:-2])
 
-
-
     # case 1 Current date - 12/13 (First column)
     if((titleyear >= 13 and titlemonth >= 12) or titleyear >= 14):
         for cell in ws['A']:
@@ -130,7 +126,6 @@ def getColumnNamesRange(currentSheet, filepath, rangevalues):
             else:
                 range.append(cell.value)
 
-
     for people in range:
         if people == "Guests":
             range.remove("Guests")
@@ -138,19 +133,75 @@ def getColumnNamesRange(currentSheet, filepath, rangevalues):
             range.remove("Guests ")
     return range
 
-def analyzePerson(person, filePath):
-    wins = 0
-    wins_2_out_of_3_teams = 0
-    lost = 0
-    total = 0
-    draws = 0
-    coeff = 0
-    coeffPoints = 0
+def analyzePersons(persons, filePath):
+    # TEST DATA ARRAY
+    persons = np.array([
+        ['name1', 10, 1, 12],
+        ['name2', 5, 3, 10],
+        ['name3', 3, 1, 11],
+        ['name4', 2, 1, 4],
+        ['name1', 7, 1, 10],
+        ['name5', 5, 2, 10],
+        ['name1', 3, 2, 5],
+        ['name2', 3, 5, 10],
+        ['name1', 5, 1, 7],
+
+        ['Clenov', 1, 0, 4], # 12/13
+        ['Clenov', 2, 1, 3], # 11/13
+                             # 10/13
+        ['Clenov', 2, 1, 4], # 09/13
+        ['Clenov', 2, 0, 4], # 08/13
+                             # 07/13
+        ['Clenov', 3, 0, 7], # 06/13
+        ['Clenov', 0, 0, 2], # 05/13
+                             # 04/13
+        ['Clenov', 0, 0, 2], # 03/13
+        ['Clenov', 1, 0, 2], # 02/13
+        ['Clenov', 2, 0, 5]  # 01/13
+    ])
+    # END TEST DATA ARRAY
 
     wb = pyxl.load_workbook(filePath)
-    sheet = wb.get_sheet_by_name(newStats)
+    statsSheet = wb.get_sheet_by_name(newStats)
 
+    # Process the list of persons
+    i = 0
+    k = 0
 
+    # for person in persons:
+    while (k < persons.shape[0]):
+        # One sheet info
+        footballplayername = persons[k,0]
 
+        # Sum player's stat and delete useless info
+        deletingindexes = list()
+        for j in range(i + 1, persons.shape[0]):
+            if (footballplayername == persons[j, 0]):
+                persons[i, 1] = int(persons[i, 1]) + int(persons[j, 1])
+                persons[i, 2] = int(persons[i, 2]) + int(persons[j, 2])
+                persons[i, 3] = int(persons[i, 3]) + int(persons[j, 3])
+                deletingindexes.append(j)
 
+        persons = np.delete(persons, deletingindexes, 0)
 
+        footballplayerwins = int(persons[k,1])
+        footballplayerdraws = int(persons[k,2])
+        footballplayernumofgames = int(persons[k,3])
+
+        footballplayervictoryrate = str(int((footballplayerwins / footballplayernumofgames) * 100)) + '%'
+        footballplayerscorerate = str(
+            int(((footballplayerwins * 3 + footballplayerdraws * 1.5) / (footballplayernumofgames * 3)) * 100)) + '%'
+
+        # Set info to the stats
+        row = i + 2
+        statsSheet.cell(row=row, column=2).value = footballplayerwins
+        statsSheet.cell(row=row, column=3).value = footballplayerdraws
+        statsSheet.cell(row=row, column=4).value = footballplayernumofgames - footballplayerwins - footballplayerdraws
+        statsSheet.cell(row=row, column=5).value = footballplayernumofgames
+        statsSheet.cell(row=row, column=6).value = footballplayervictoryrate
+        statsSheet.cell(row=row, column=7).value = footballplayerscorerate
+
+        i += 1
+        k += 1
+
+    wb.save(filename=filePath)
