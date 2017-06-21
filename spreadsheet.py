ce1c9f0951b148744f5b3d0391ff0f1576cb0ba5
Max Pavlukhin <pavlukhinm@gmail.com>
2017-06-21 17:28:52 +0400

    Some bugs was fixed

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,6 +1,5 @@
 import urllib.request
 
-
 # Download spreadsheet from Google Drive Service
 def download_sheet():
     url = 'https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=1ReoKdwH_t62mnRHEae3f8yq_gYtKH8gTjzlWDOt0TUU&exportFormat=xlsx'
