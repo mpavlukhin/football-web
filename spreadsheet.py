f16cb7af224cfae9b7b561441ebde059637fe275
Max Pavlukhin <pavlukhinm@gmail.com>
2018-04-29 14:39:23 +0400

    Change Google Spreadsheet Link

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,8 +1,7 @@
 import urllib.request
 
 
-def download_sheet(url='https://spreadsheets.google.com/feeds/download/'
-                       'spreadsheets/Export?key=1ReoKdwH_t62mnRHEae3f8'
-                       'yq_gYtKH8gTjzlWDOt0TUU&exportFormat=xlsx',
+def download_sheet(url='https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=1QH-AFYHk3lXJf-dG3FzhDwt'
+                       'O6iJZus7ZXWoY8aBs7ZI&exportFormat=xlsx',
                    file_name='data/spreadsheets/Football-bigdata-v0.2.xlsx'):
     urllib.request.urlretrieve(url, file_name)
